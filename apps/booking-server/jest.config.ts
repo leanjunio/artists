26f99db161d10107853de59778b4c9d827553e26
Lean Junio <leanjunio@outlook.com>
2023-07-11 15:10:54 -0400

    update

--- apps/booking-server/jest.config.ts
+++ apps/booking-server/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'artists',
+  displayName: 'booking-server',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/artists',
+  coverageDirectory: '../../coverage/apps/booking-server',
 };
